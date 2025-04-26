e0535ec53f80a755a51fa79195ae3dc8c4e24a1b
kjgdhrhrrgg <44415820+kjgdhrhrrgg@users.noreply.github.com>
2025-04-26 16:07:19 +0200

    remove test files

--- check_discord.py
+++
@@ -1,7 +0,0 @@
-import discord
-
-print("Pycord version:", discord.__version__)
-print("\nAttributes in discord module:\n")
-
-for attr in dir(discord):
-    print(attr)
--- test_discord.py
+++
@@ -1,4 +0,0 @@
-import discord
-
-print("Pycord version:", discord.__version__)
-print("Has app_commands:", hasattr(discord, "app_commands"))
